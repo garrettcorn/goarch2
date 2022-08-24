f85659e39e40c2352783dc4eaabe207f2d33c083
Garrett Cornwell <5914976+garrettcorn@users.noreply.github.com>
2022-08-24 16:12:36 -0400

    ninja 02 hoe 04

--- client/main.go
+++ client/main.go
@@ -1,138 +1,328 @@
 package main
 
 import (
+	"crypto/hmac"
 	"crypto/sha256"
-	"encoding/hex"
-	"encoding/json"
+	"encoding/base64"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
+	"time"
 
-	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
-type Config struct {
-	Users    []Person `json:"Users"`
-	Port     string   `json:"Port"`
-	Filename string   `json:"-"`
+type User struct {
+	Name     string
+	Password []byte
 }
 
-type Person struct {
-	First string `json:"first"`
-	Last  string `json:"last"`
+type Session struct {
+	Id        string
+	Email     string
+	ExpiresAt time.Time
 }
 
-func (p Person) String() string {
-	return fmt.Sprintf("First name is: %s. Last name is: %s.", p.First, p.Last)
+type SessionStore interface {
+	Add(s Session)
+	Remove(id string)
+	Get(id string) (Session, error)
 }
 
-func (c Config) Write() (n int, err error) {
-	xb, err := json.MarshalIndent(c, "", "\t")
+type Storage interface {
+	SaveUser(u User) error
+	GetUser(username string) User
+}
+
+type MemorySessionStore struct {
+	sessionStore map[string]Session
+}
+
+func NewMemorySessionStore() *MemorySessionStore {
+	return &MemorySessionStore{
+		sessionStore: map[string]Session{},
+	}
+}
+
+func (ss *MemorySessionStore) Add(s Session) {
+	ss.sessionStore[s.Id] = s
+}
+
+func (ss *MemorySessionStore) Remove(id string) {
+	delete(ss.sessionStore, id)
+}
+
+func (ss MemorySessionStore) Get(id string) (Session, error) {
+	if time.Now().Before(ss.sessionStore[id].ExpiresAt) {
+		return ss.sessionStore[id], nil
+	} else {
+		delete(ss.sessionStore, id)
+	}
+	return Session{}, fmt.Errorf("session not found")
+}
+
+type MemoryStore struct {
+	store map[string][]byte
+}
+
+func NewMemoryStore() *MemoryStore {
+	return &MemoryStore{
+		store: map[string][]byte{},
+	}
+}
+
+var ErrUserAlreadyRegistered = errors.New("MemoryStore: user aleady registered")
+
+func (s *MemoryStore) SaveUser(u User) error {
+	if s.store[u.Name] == nil {
+		s.store[u.Name] = []byte(u.Password)
+		return nil
+	}
+	return ErrUserAlreadyRegistered
+}
+
+func (s *MemoryStore) GetUser(username string) User {
+	return User{Name: username, Password: s.store[username]}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	var loggedInText string
+	c, err := r.Cookie("session")
 	if err != nil {
-		log.Fatalf("Unable to marshall. %v\n", err)
-	}
-	f, err := os.Create(c.Filename)
-	if err != nil {
-		return 0, err
-	}
-	defer f.Close()
-	if err != nil {
-		return 0, fmt.Errorf("unable to write config file. %e", err)
-	}
-	return f.Write(xb)
-}
-
-func (c *Config) Read() error {
-	f, err := os.Open(c.Filename)
-	if err != nil {
-		return fmt.Errorf("unable to read config. %e", err)
-	}
-	defer f.Close()
-	d := json.NewDecoder(f)
-	return d.Decode(c)
-}
-
-func (c *Config) SetDefaults() {
-	if c.Filename == "" {
-		c.Filename = "config.json"
-	}
-	if c.Port == "" {
-		c.Port = ":4040"
-	}
-	if len(c.Users) == 0 {
-		c.Users = []Person{
-			{
-				First: "John",
-				Last:  "Doe",
-			},
-			{
-				First: "Jane",
-				Last:  "Doe",
-			},
+		log.Println("index unable to get session cookie: " + err.Error())
+	} else {
+		ps, err := parseToken(c.Value, key)
+		if err != nil {
+			log.Println("index unable to parse the session token: " + err.Error())
+		} else {
+			s, err := sessionStore.Get(ps)
+			if err != nil {
+				log.Println("index unable to get session from session store: " + err.Error())
+			} else {
+				loggedInText = "Logged in as: " + s.Email
+			}
 		}
 	}
+
+	statusMsg := r.FormValue("statusMsg")
+	html := `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+	<h2>` + loggedInText + `</h2>
+	<h2>Register</h2>
+    <form action="/register" method="post" class="form-example">
+        <div class="form-example">
+          <label for="username">Enter your username: </label>
+          <input type="text" name="username" id="username" required>
+        </div>
+        <div class="form-example">
+            <label for="password">Enter your password: </label>
+            <input type="password" name="password" id="password" required>
+          </div>
+        <div class="form-example">
+          <input type="submit" value="Register">
+        </div>
+    </form>
+	<h2>Login</h2>
+    <form action="/login" method="post" class="form-example">
+        <div class="form-example">
+          <label for="username">Enter your username: </label>
+          <input type="text" name="username" id="username" required>
+        </div>
+        <div class="form-example">
+            <label for="password">Enter your password: </label>
+            <input type="password" name="password" id="password" required>
+          </div>
+        <div class="form-example">
+          <input type="submit" value="Login">
+        </div>
+    </form>
+	<p>` + statusMsg + `</p>
+</body>
+</html>`
+	w.Write([]byte(html))
 }
 
-func encodepp(w http.ResponseWriter, r *http.Request) {
-	e := json.NewEncoder(w)
-	e.SetIndent("", "\t")
-	e.SetEscapeHTML(true)
-	e.Encode(c)
+func encrypt(password []byte) ([]byte, error) {
+	return bcrypt.GenerateFromPassword(password, bcrypt.MinCost)
 }
 
-func encode(w http.ResponseWriter, r *http.Request) {
-	e := json.NewEncoder(w)
-	e.SetIndent("", "")
-	e.SetEscapeHTML(true)
-	e.Encode(c)
-}
-
-func decode(w http.ResponseWriter, r *http.Request) {
-	err := json.NewDecoder(r.Body).Decode(&c)
-	if err != nil {
-		log.Println(err)
+func register(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		statusMsg := url.QueryEscape("method needs to be post")
+		http.Redirect(w, r, "/?statusMsg="+statusMsg, http.StatusSeeOther)
+		return
 	}
-	fmt.Println("got:", c)
-	c.Write()
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	if username != "" && password != "" {
+		ePassword, err := encrypt([]byte(password))
+		if err != nil {
+			statusMsg := url.QueryEscape("unable to encrypt password")
+			http.Redirect(w, r, "/?statusMsg="+statusMsg, http.StatusSeeOther)
+			return
+		}
+		err = storage.SaveUser(User{Name: username, Password: ePassword})
+		if err != nil {
+			statusMsg := url.QueryEscape("unable to save user")
+			http.Redirect(w, r, "/?statusMsg="+statusMsg, http.StatusSeeOther)
+			return
+		}
+		uuid, err := uuid.NewUUID()
+		if err != nil {
+			statusMsg := url.QueryEscape("unable to create uuid for user")
+			http.Redirect(w, r, "/?statusMsg="+statusMsg, http.StatusSeeOther)
+			return
+		}
+		id := uuid.String()
+		expAt := time.Now().Add(5 * time.Second)
+		sessionStore.Add(Session{
+			Id:        id,
+			Email:     username,
+			ExpiresAt: expAt,
+		})
+		signedSession, err := createToken([]byte(id), key)
+		if err != nil {
+			statusMsg := url.QueryEscape("unable to sign session")
+			http.Redirect(w, r, "/?statusMsg="+statusMsg, http.StatusSeeOther)
+			return
+		} else {
+			http.SetCookie(w,
+				&http.Cookie{
+					Name:    "session",
+					Value:   signedSession,
+					Expires: expAt,
+				})
+		}
+	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
-var c Config
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		statusMsg := url.QueryEscape("method needs to be post")
+		http.Redirect(w, r, "/?statusMsg="+statusMsg, http.StatusSeeOther)
+		return
+	}
 
-func sha256Example(msg string) string {
-	hashF := sha256.New()
-	hashF.Write([]byte(msg))
-	hashS := hex.EncodeToString(hashF.Sum(nil))
-	return string(hashS)
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	if username != "" && password != "" {
+		u := storage.GetUser(username)
+		fmt.Printf("password: %v\n", password)
+		fmt.Printf("u.Password: %v\n", u.Password)
+		if bcrypt.CompareHashAndPassword(u.Password, []byte(password)) == nil {
+			statusMsg := url.QueryEscape("login successful")
+
+			uuid, err := uuid.NewUUID()
+			if err != nil {
+				statusMsg := url.QueryEscape("unable to create uuid for user")
+				http.Redirect(w, r, "/?statusMsg="+statusMsg, http.StatusSeeOther)
+				return
+			}
+			id := uuid.String()
+			expAt := time.Now().Add(5 * time.Second)
+			sessionStore.Add(Session{
+				Id:        id,
+				Email:     username,
+				ExpiresAt: expAt,
+			})
+			signedSession, err := createToken([]byte(id), key)
+			if err != nil {
+				statusMsg := url.QueryEscape("unable to sign session")
+				http.Redirect(w, r, "/?statusMsg="+statusMsg, http.StatusSeeOther)
+				return
+			} else {
+				http.SetCookie(w,
+					&http.Cookie{
+						Name:    "session",
+						Value:   signedSession,
+						Expires: expAt,
+					})
+			}
+
+			http.Redirect(w, r, "/?statusMsg="+statusMsg, http.StatusSeeOther)
+			return
+		} else {
+			log.Println("faled login attempt from:", r.RemoteAddr)
+		}
+	}
+	statusMsg := url.QueryEscape("login failed")
+	http.Redirect(w, r, "/?statusMsg="+statusMsg, http.StatusSeeOther)
 }
 
-type UserClaims struct {
-	jwt.StandardClaims
-	SessionId int64
+func hmacSign(msg []byte, key []byte) (string, error) {
+	h := hmac.New(sha256.New, key)
+	_, err := h.Write(msg)
+	if err != nil {
+		return "", fmt.Errorf("hmacSign: unable to write msg to hash. %w", err)
+	}
+	return base64.URLEncoding.EncodeToString(h.Sum(nil)), nil
 }
 
+func hmacCheck(msg, sig, key []byte) bool {
+	tempSig, err := hmacSign(msg, key)
+	if err != nil {
+		return false
+	}
+	return hmac.Equal([]byte(tempSig), sig)
+}
+
+func createToken(sid, key []byte) (string, error) {
+	sig, err := hmacSign(sid, key)
+	if err != nil {
+		return "", fmt.Errorf("cToken: unable to hmacSign %s, because %s", sid, err)
+	}
+	return string(sig) + "|" + string(sid), nil
+}
+
+func parseToken(ss string, key []byte) (string, error) {
+	sep := "|"
+	xs := strings.SplitN(ss, sep, 2)
+	if len(xs) != 2 {
+		return "", fmt.Errorf("length not equal to 2")
+	}
+	sig := xs[0]
+	sid := xs[1]
+	if hmacCheck([]byte(sid), []byte(sig), key) {
+		return sid, nil
+	}
+	return "", fmt.Errorf("unable to parse token")
+}
+
+var storage Storage
+var sessionStore SessionStore
 var key []byte
 
-func createJWT(c *UserClaims) (string, error) {
-	t := jwt.NewWithClaims(jwt.SigningMethodHS512, c)
-	return t.SignedString(t)
-}
-
 func main() {
-	c.Read()
-	fmt.Println("config:\n", c)
-	c.SetDefaults()
-	defer c.Write()
+	storage = NewMemoryStore()
+	sessionStore = NewMemorySessionStore()
+	key = []byte("MySuperSecretKey")
+	http.HandleFunc("/", index)
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
 
-	fmt.Println("hash is:", sha256Example("hello"))
-
-	http.HandleFunc("/encodepp", encodepp)
-	http.HandleFunc("/encode", encode)
-	http.HandleFunc("/decode", decode)
-
-	err := http.ListenAndServe(c.Port, nil)
+	ss, err := createToken([]byte("somerandomsid"), key)
 	if err != nil {
-		panic(err)
+		log.Println("unable to create token", err)
+	}
+	sid, err := parseToken(ss, key)
+	if err != nil {
+		log.Printf("unable to parse. %v\n", err)
+	} else {
+		log.Println("token created and parsed successfully. sid=" + sid)
 	}
 
+	log.Panic(http.ListenAndServe(":9090", nil))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/garrettcorn/goarch2
 
 go 1.19
 
-require github.com/golang-jwt/jwt/v4 v4.4.2
+require (
+	github.com/google/uuid v1.3.0
+	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8
+)
